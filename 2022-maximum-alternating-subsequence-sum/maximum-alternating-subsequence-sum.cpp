d0d59e74ea461fb031d8f9d68b42ae7a089155a7
ScrewedUP <43698882+ScrewedUP@users.noreply.github.com>
2024-03-13 10:34:46 +0530

    Time: 170 ms (46.02%) | Memory: 104.7 MB (54.96%) - LeetSync

--- 2022-maximum-alternating-subsequence-sum/maximum-alternating-subsequence-sum.cpp
+++ 2022-maximum-alternating-subsequence-sum/maximum-alternating-subsequence-sum.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
-    long long t[1000005][2];
+    long long t[2][100005];
     long long solve(int idx,int last,vector<int> &nums){
         if ( idx >= nums.size() ) return 0;
-        if ( last != -1 && t[idx][last] != -1 ) return t[idx][last];
+        if ( last != -1 && t[last][idx] != -1 ) return t[last][idx];
         long long take = 0;
         if ( last == -1 || last == 0 ){
             take = nums[idx] + solve(idx+1,1,nums);
         }
         else take = -1*nums[idx] + solve(idx+1,0,nums);
         long long notTake = solve(idx+1,last,nums);
-        if ( last != -1 ) t[idx][last] = max(take,notTake);
+        if ( last != -1 ) t[last][idx] = max(take,notTake);
         return max(take,notTake);
     }
     long long maxAlternatingSum(vector<int>& nums) {
