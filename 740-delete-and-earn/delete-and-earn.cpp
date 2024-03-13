bb5d0644bf8afcc50a18da4fc078c42296d77f60
ScrewedUP <43698882+ScrewedUP@users.noreply.github.com>
2024-03-13 14:15:22 +0530

    Time: 287 ms (5.03%) | Memory: 104.3 MB (5.03%) - LeetSync

--- 740-delete-and-earn/delete-and-earn.cpp
+++ 740-delete-and-earn/delete-and-earn.cpp
@@ -2,17 +2,17 @@
 class Solution {
 public:
     int deleteAndEarn(vector<int>& nums) {
-        map<int,int>m;
-        int n= nums.size();
+        map<int,int> m;
+        int n = nums.size();
         for (int i=0;i<n;++i){
             m[nums[i]]++;
         }
-        vector<ll>dp(1e5+1);
-        dp[0]=0;
-        dp[1]=m[1];
-        for (int i=2;i<=1e5;++i){
-            dp[i]=max(dp[i-1],dp[i-2]+1ll*m[i]*i);
+        vector<ll>dp(2e4+1);
+        dp[0] = 0;
+        dp[1] = m[1];
+        for (int i = 2 ; i <= 2e4; i++){
+            dp[i] = max(dp[i-1],dp[i-2]+ m[i]*i);
         }
-        return dp[1e5];
+        return dp[2e4];
     }
 };
