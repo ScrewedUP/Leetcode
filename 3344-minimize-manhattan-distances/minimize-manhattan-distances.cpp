b9177b04d842513cffbe8cbbb448d545c197db09
ScrewedUP <43698882+ScrewedUP@users.noreply.github.com>
2024-03-31 10:15:12 +0530

    Time: 659 ms (33.33%) | Memory: 158 MB (66.67%) - LeetSync

--- 3344-minimize-manhattan-distances/minimize-manhattan-distances.cpp
+++ 3344-minimize-manhattan-distances/minimize-manhattan-distances.cpp
@@ -13,9 +13,9 @@ public:
             if ( a[points[i][0] + points[i][1]] == 0 ) a.erase(points[i][0] + points[i][1]);
             b[points[i][0] - points[i][1]]--;
             if ( b[points[i][0] - points[i][1]] == 0 ) b.erase(points[i][0] - points[i][1]);
-            if (!a.empty() && !b.empty()) {
+            // if (!a.empty() && !b.empty()) {
                 ans = min(ans, max(a.rbegin()->first - a.begin()->first, b.rbegin()->first-b.begin()->first));
-            }
+            // }
             a[points[i][0] + points[i][1]]++;
             b[points[i][0] - points[i][1]]++;
         }
