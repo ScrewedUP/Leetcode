1bfa2e4bf1fbcbb40df21cac0b2669f5bb78244b
ScrewedUP <43698882+ScrewedUP@users.noreply.github.com>
2024-04-01 15:19:34 +0530

    Time: 0 ms (100.00%) | Memory: 7.6 MB (57.50%) - LeetSync

---
+++ 58-length-of-last-word/length-of-last-word.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int cnt = 0;
+        for(int i = s.size() - 1 ; i >= 0 ; i--){
+            if ( s[i] == ' ' && cnt == 0 ) continue;
+            else if ( s[i] == ' '  ) return cnt;
+            else cnt++;
+        }
+        return cnt;
+    }
+};
