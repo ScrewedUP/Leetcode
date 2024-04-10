4fae2883cacddd286b6c062239053c853f58363d
ScrewedUP <43698882+ScrewedUP@users.noreply.github.com>
2024-04-10 12:10:40 +0530

    Time: 53 ms (18.53%) | Memory: 28.6 MB (5.03%) - LeetSync

---
+++ 987-reveal-cards-in-increasing-order/reveal-cards-in-increasing-order.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> deckRevealedIncreasing(vector<int>& deck) {
+        sort(deck.begin(),deck.end(),greater<int>());
+        deque<int> q;
+        q.push_back(deck[0]);
+        for(int i = 1 ; i < deck.size() ; i++){
+            for(int j = 0 ; j < i-1 ; j++){
+                auto curr = q[0];
+                q.pop_front();
+                q.push_back(curr);
+            }
+            q.push_front(deck[i]);
+            // for(auto it : q){
+            //     cout << it << " ";
+            // }
+            // cout << endl;
+        }
+        vector<int> ans;
+        for(auto it : q){
+            ans.push_back(it);
+        }
+        return ans;
+    }
+};
