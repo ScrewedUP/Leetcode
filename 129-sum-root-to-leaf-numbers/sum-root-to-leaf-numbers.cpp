2ad93db2a23d2002d3b436966fd11aa99901f40e
ScrewedUP <43698882+ScrewedUP@users.noreply.github.com>
2024-04-15 09:38:04 +0530

    Time: 7 ms (26.49%) | Memory: 10.8 MB (96.99%) - LeetSync

--- 129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.cpp
+++ 129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.cpp
@@ -13,29 +13,19 @@ class Solution {
 public:
     int ans = 0;
     void dfs(TreeNode* root , int val){
-        // if ( root == NULL){
-        //     cout << val << " ";
-        //     ans += val;
-        //     return;
-        // }
-        
         if ( root->left) dfs(root->left,val*10 + root->val);
-        // else val = val*10 + root->val;
         if ( root->right) dfs(root->right,val*10 + root->val);
         val = val*10 + root->val;
         if ( root->left == NULL && root->right == NULL){
-            cout << val << " ";
             ans += val;
         }
         
     }
-    //49 491 491 40 40 
     int sumNumbers(TreeNode* root) {
         if ( root == NULL ) return ans;
         if ( root->left == NULL && root->right == NULL) return root->val;
         if ( root->left) dfs(root->left,root->val);
         if ( root->right) dfs(root->right,root->val);
-        if ( ans == 0 ) return root->val;
         return ans;
     }
 };
