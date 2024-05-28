496c76eb47bccf21579eb27775fd99984a71fb60
ScrewedUP <43698882+ScrewedUP@users.noreply.github.com>
2024-05-28 08:48:13 +0530

    Time: 4 ms (85.51%) | Memory: 8.8 MB (70.28%) - LeetSync

--- 1321-get-equal-substrings-within-budget/get-equal-substrings-within-budget.cpp
+++ 1321-get-equal-substrings-within-budget/get-equal-substrings-within-budget.cpp
@@ -15,13 +15,5 @@ public:
             maxi = max(maxi,right - left + 1);
         }
         return maxi;
-        // if ( maxi != 0 ) return maxi;
-        // int p = 0;
-        // int cnt = 0;
-        // while ( s[p] == t[p] && p < s.size()){
-        //     cnt++;
-        //     p++;
-        // }
-        // return cnt;
     }
 };
