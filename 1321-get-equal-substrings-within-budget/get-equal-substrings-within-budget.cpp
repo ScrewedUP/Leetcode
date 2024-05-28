e0ebe509cecf94d3a3a37b565d05978224b3c25e
ScrewedUP <43698882+ScrewedUP@users.noreply.github.com>
2024-05-28 08:47:28 +0530

    Time: 3 ms (97.07%) | Memory: 8.9 MB (37.19%) - LeetSync

---
+++ 1321-get-equal-substrings-within-budget/get-equal-substrings-within-budget.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int equalSubstring(string s, string t, int maxCost) {
+        int maxi = 0;
+        int left = 0;
+        int sum = 0;
+        for(int right = 0; right < s.size() ; right++){
+            if ( s[right] != t[right]){
+                sum += abs(s[right] - t[right]);
+            }
+            while ( sum > maxCost){
+                sum -= abs(s[left] - t[left]);
+                left++;
+            }
+            maxi = max(maxi,right - left + 1);
+        }
+        if ( maxi != 0 ) return maxi;
+        int p = 0;
+        int cnt = 0;
+        while ( s[p] == t[p] && p < s.size()){
+            cnt++;
+            p++;
+        }
+        return cnt;
+    }
+};
