b6d83346f92e636fe125bf1f4c21c6782df44e8a
ScrewedUP <43698882+ScrewedUP@users.noreply.github.com>
2024-06-04 08:24:24 +0530

    Time: 3 ms (55.35%), Space: 9 MB (48.94%) - LeetHub

---
+++ 0409-longest-palindrome/0409-longest-palindrome.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int longestPalindrome(string s) {
+        map<char,int> m;
+        for(auto it : s) m[it]++;
+        int cnt = 0;
+        bool there = false;
+        for(auto it : m ){
+            if ( it.second >= 2){
+                if ( it.second & 1 ){
+                    cnt += it.second - 1;
+                }
+                else{
+                    cnt += it.second;
+                }
+            }
+            if ( it.second == 1 || it.second & 1 ) there = true;
+        }
+        if ( there ){
+            return cnt + 1;
+        }
+        else return cnt;
+    }
+};
