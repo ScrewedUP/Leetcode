d4a8f74ae06bd69e286a3f842f307aa67bf95158
ScrewedUP <43698882+ScrewedUP@users.noreply.github.com>
2024-06-06 12:26:29 +0530

    Time: 69 ms (29.97%), Space: 30.7 MB (54.6%) - LeetHub

---
+++ 0876-hand-of-straights/0876-hand-of-straights.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isNStraightHand(vector<int>& hand, int groupSize) {
+       int n = hand.size();
+
+       map<int,int> m;
+       for(auto it : hand){
+        m[it]++;
+       } 
+       while ( !m.empty()){
+        int curr = m.begin()->first;
+
+        for(int i = 0 ; i < groupSize ; i++){
+            if ( m[curr + i] == 0 ) return false;
+            m[curr + i]--;
+
+            if ( m[curr + i] == 0 ){
+                m.erase(curr + i);
+            }
+        } 
+       }
+       return true;
+    }
+};
