034b5ea87e0a8731a421af0aef71521e1ff27687
ScrewedUP <43698882+ScrewedUP@users.noreply.github.com>
2024-06-18 09:36:18 +0530

    Time: 53 ms (51.93%) | Memory: 41.3 MB (47.29%) - LeetSync

--- 853-most-profit-assigning-work/most-profit-assigning-work.cpp
+++ 853-most-profit-assigning-work/most-profit-assigning-work.cpp
@@ -1,41 +1,3 @@
-class SegmentTree {
-    private:
-        vector<int> tree;
-        int n;
-
-        void buildTree(const vector<int>& profits, int idx, int l, int r) {
-            if (l == r) {
-                tree[idx] = profits[l];
-            } else {
-                int mid = (l + r) / 2;
-                buildTree(profits, 2 * idx + 1, l, mid);
-                buildTree(profits, 2 * idx + 2, mid + 1, r);
-                tree[idx] = max(tree[2 * idx + 1], tree[2 * idx + 2]);
-            }
-        }
-
-        int queryTree(int idx, int l, int r, int ql, int qr) {
-            if (ql > r || qr < l) {
-                return 0;
-            }
-            if (ql <= l && qr >= r) {
-                return tree[idx];
-            }
-            int mid = (l + r) / 2;
-            return max(queryTree(2 * idx + 1, l, mid, ql, qr), queryTree(2 * idx + 2, mid + 1, r, ql, qr));
-        }
-
-    public:
-        SegmentTree(const vector<int>& profits) {
-            n = profits.size();
-            tree.resize(4 * n, 0);
-            buildTree(profits, 0, 0, n - 1);
-        }
-
-        int query(int l, int r) {
-            return queryTree(0, 0, n - 1, l, r);
-        }
-    };
 class Solution {
 public:
     int maxProfitAssignment(vector<int>& difficulty, vector<int>& profit, vector<int>& worker) {
@@ -50,7 +12,6 @@ public:
         for (int i = 1; i < n; i++) {
             maxi[i] = max(maxi[i-1],vp[i].second);
         }
-        // SegmentTree segTree(profits);
         int ans = 0;
         for(int i = 0 ; i < worker.size() ; i++){
             int idx = upper_bound(vp.begin(), vp.end(), make_pair(worker[i], INT_MAX), 
