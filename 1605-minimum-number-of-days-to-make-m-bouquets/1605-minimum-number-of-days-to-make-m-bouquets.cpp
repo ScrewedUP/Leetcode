c1c6a45ec0a00846a7b011c106c632156073efab
ScrewedUP <43698882+ScrewedUP@users.noreply.github.com>
2024-06-19 09:22:41 +0530

    Time: 209 ms (5.04%), Space: 145.2 MB (5.23%) - LeetHub

---
+++ 1605-minimum-number-of-days-to-make-m-bouquets/1605-minimum-number-of-days-to-make-m-bouquets.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    bool canPlace(int mid , vector<int>& v, int m, int k ){
+        vector<int> can(v.size(),0);
+        for(int i = 0 ; i < v.size() ; i++){
+            if ( v[i] <= mid ){
+                can[i] = 1;
+            }
+        }
+        int cnt = 0;
+        int count = 0;
+        for(int i = 0 ; i < v.size() ; i++){
+            if ( can[i] == 1){
+                count++;
+                if ( count == k){
+                    cnt++;
+                    count = 0;
+                }
+            }
+            else count = 0;
+        }
+        if ( cnt >= m ) return true;
+        return false;
+    }
+    int minDays(vector<int>& bloomDay, int m, int k) {
+        // sort(bloomDay.begin(),bloomDay.end());
+        if ( (long long)m*k > bloomDay.size() ) return -1;
+        int low = 1;
+        int high = 1e9;
+        int ans = 0;
+        while( low <= high ){
+            int mid = low + (high - low)/2;
+            if ( canPlace(mid,bloomDay,m,k) ){
+                ans = mid ;
+                high = mid - 1;
+            }
+            else low = mid + 1;
+        }
+        return ans;
+    }
+};
