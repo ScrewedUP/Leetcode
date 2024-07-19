6f138356a8f1eebedc8ffd360872f74c458f667e
ScrewedUP <43698882+ScrewedUP@users.noreply.github.com>
2024-07-19 21:24:32 +0530

    Time: 536 ms (16.12%), Space: 255 MB (16.7%) - LeetHub

--- 2832-find-the-longest-equal-subarray/2832-find-the-longest-equal-subarray.cpp
+++ 2832-find-the-longest-equal-subarray/2832-find-the-longest-equal-subarray.cpp
@@ -1,21 +1,37 @@
 class Solution {
 public:
     int longestEqualSubarray(vector<int>& nums, int k) {
-        unordered_map<int, vector<int>> m;
-        for (int i = 0; i < nums.size(); ++i) {
+        map<int, vector<int>> m;
+        for (int i = 0; i < nums.size(); i++) {
             m[nums[i]].push_back(i);
         }
 
         int maxi = 0;
 
-        for (auto& [num, indices] : m) {
-            int left = 0;
-            for (int right = 0; right < indices.size(); ++right) {
-                while (indices[right] - indices[left] - (right - left) > k) {
-                    left++;
+        for (auto it : m) {
+            int n = it.second.size();
+            int j = k;
+            int currMaxi = 1;
+            int globalMaxi = 1;
+
+            for (int i = 1; i < n; i++) {
+                int gap = it.second[i] - it.second[i - 1] - 1;
+
+                if (gap <= j) {
+                    currMaxi++;
+                    j -= gap;
+                } else {
+                    globalMaxi = max(globalMaxi, currMaxi);
+                    while (j < gap && currMaxi > 0) {
+                        currMaxi--;
+                        j += it.second[i - currMaxi] - it.second[i - currMaxi - 1] - 1;
+                    }
+                    currMaxi++;
+                    j -= gap;
                 }
-                maxi = max(maxi, right - left + 1);
             }
+            globalMaxi = max(globalMaxi, currMaxi);
+            maxi = max(maxi, globalMaxi);
         }
 
         return maxi;
