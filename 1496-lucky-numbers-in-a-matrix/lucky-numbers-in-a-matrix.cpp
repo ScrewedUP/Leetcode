6434e633cf1c799790aa7e7edabb74eb6c3e6e4e
ScrewedUP <43698882+ScrewedUP@users.noreply.github.com>
2024-07-19 10:17:51 +0530

    Time: 30 ms (5.32%) | Memory: 14.3 MB (18.75%) - LeetSync

---
+++ 1496-lucky-numbers-in-a-matrix/lucky-numbers-in-a-matrix.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> luckyNumbers (vector<vector<int>>& mat) {
+        vector<int> ans;
+        int n = mat.size();
+        int m = mat[0].size();
+        for(int i = 0 ; i < n ; i++ ){
+            for(int j = 0 ; j < m ; j++){
+                int mini = *min_element(mat[i].begin(),mat[i].end());
+                int maxi = INT_MIN;
+                for(int k = 0 ; k < n ; k++){
+                    maxi = max(maxi,mat[k][j]);
+                }
+                if ( mat[i][j] == mini && mat[i][j] == maxi){
+                    ans.push_back(mat[i][j]);
+                }
+            }
+        }
+        return ans;
+    }
+};
