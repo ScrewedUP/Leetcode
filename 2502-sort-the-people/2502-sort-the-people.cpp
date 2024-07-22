4ec2ca75d23c960dcaf6157105d6f1f582c60c48
ScrewedUP <43698882+ScrewedUP@users.noreply.github.com>
2024-07-22 08:51:29 +0530

    Time: 28 ms (64.96%), Space: 30.4 MB (8.15%) - LeetHub

---
+++ 2502-sort-the-people/2502-sort-the-people.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<string> sortPeople(vector<string>& names, vector<int>& heights) {
+        vector<pair<int,string>> vp;
+        for(int i = 0 ; i < names.size() ; i++){
+            vp.push_back({heights[i],names[i]});
+        }
+        sort(vp.rbegin(),vp.rend());
+        vector<string> ans;
+        for(int i = 0 ; i < vp.size() ; i++){
+            ans.push_back(vp[i].second);
+        }
+        return ans;
+    }
+};
