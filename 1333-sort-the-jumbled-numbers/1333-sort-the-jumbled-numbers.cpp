add41d51972396ad2c39f76a5c15765292c93612
ScrewedUP <43698882+ScrewedUP@users.noreply.github.com>
2024-07-24 09:18:04 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1333-sort-the-jumbled-numbers/1333-sort-the-jumbled-numbers.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    vector<int> sortJumbled(vector<int>& mapping, vector<int>& nums) {
+        map<int,int> m;
+        for(int i = 0 ; i < mapping.size() ; i++){
+            m[i] = mapping[i];
+        }
+        map<int,int> mp;
+        for(int i = 0 ;i < nums.size() ; i++){
+            int curr = nums[i];
+            string currNum = to_string(curr);
+            for(int j = 0 ; j < currNum.size() ; j++){
+                if ( m.find(currNum[j] - '0') != m.end()){
+                    int ch = m[currNum[j] - '0'];
+                    currNum[j] = ch + '0';
+                }
+            }
+            int x = 0;
+            while ( currNum[x] == '0'){
+                x++;
+            }
+            if ( x > 0){
+                string newStr = "";
+                for(int k = x ; k < currNum.size() ; k++){
+                    newStr += currNum[k];
+                }
+                int ny = stoi(newStr);
+                mp[nums[i]] = ny;
+            }
+            else{
+                int ny = stoi(currNum);
+                mp[nums[i]] = ny;
+            }
+        }
+        for(auto it : nums) cout << it <<  " ";
+        sort(nums.begin(),nums.end(),[&mp](int &a, int &b){
+            return mp[a] < mp[b];
+        });
+        return nums;
+    }
+};
