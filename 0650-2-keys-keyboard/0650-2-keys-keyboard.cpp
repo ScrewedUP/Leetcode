235d0c150ffe8e3497e28d9e9b2bba5125d0dbef
ScrewedUP <43698882+ScrewedUP@users.noreply.github.com>
2024-08-19 10:06:11 +0530

    Time: 13 ms (39.85%), Space: 12 MB (24.18%) - LeetHub

--- 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
@@ -1,14 +1,17 @@
 class Solution {
 public:
+    int t[1001][1001];
     int solve(int c,int n,int l){
         if ( c > n ) return 1e4;
         if ( c == n ) return 0;
+        if ( t[c][l] != -1 ) return t[c][l];
         int paste1 = 1 + solve(c + l,n,l);
         int paste2 = 2 + solve( c + c , n, c);
-        return min(paste1,paste2);
+        return t[c][l] = min(paste1,paste2);
     }
     int minSteps(int n) {
         if ( n == 1 ) return 0;
+        memset(t,-1,sizeof(t));
         return 1 + solve(1,n,1);
     }
 };
