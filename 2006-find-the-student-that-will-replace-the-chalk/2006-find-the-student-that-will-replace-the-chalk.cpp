fea3af6fdfeca5379ba520070f0010ee9233d154
ScrewedUP <43698882+ScrewedUP@users.noreply.github.com>
2024-09-02 09:12:02 +0530

    Time: 92 ms (73.47%), Space: 77 MB (22.4%) - LeetHub

---
+++ 2006-find-the-student-that-will-replace-the-chalk/2006-find-the-student-that-will-replace-the-chalk.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int chalkReplacer(vector<int>& chalk, int k) {
+        long long sum = 0;
+        for(auto it : chalk) sum += it;
+        int times = k / sum;
+        int rem =  k - times*sum;
+        for(int i = 0 ; i < chalk.size() ; i++){
+            if ( chalk[i] > rem ){
+                return i;
+            }
+            else{
+                rem -= chalk[i];
+            }
+        }
+        return 0;
+    }
+};
