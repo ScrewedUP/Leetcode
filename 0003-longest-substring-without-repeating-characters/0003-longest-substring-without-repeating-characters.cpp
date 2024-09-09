8ed2e8dadc267fcce79a36678a43c627c8584292
ScrewedUP <43698882+ScrewedUP@users.noreply.github.com>
2024-09-09 22:34:11 +0530

    Time: 10 ms (71.86%), Space: 12.1 MB (57.38%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -6,7 +6,7 @@ public:
         unordered_map<char,int> m;
         for(int right = 0; right < s.size() ;right++){
             m[s[right]]++;
-            while( m[s[right]] > 1 ){
+            while( m[s[right]] == 2 ){
                 m[s[left]]--;
                 left++;
             }
