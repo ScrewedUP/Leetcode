983b24c2a6d777a496a8af22eedc73391b8b050e
ScrewedUP <43698882+ScrewedUP@users.noreply.github.com>
2024-12-06 10:25:47 +0530

    Time: 4 ms (99.24%) | Memory: 128.1 MB (72.54%) - LeetSync

--- 2640-maximum-number-of-integers-to-choose-from-a-range-i/maximum-number-of-integers-to-choose-from-a-range-i.cpp
+++ 2640-maximum-number-of-integers-to-choose-from-a-range-i/maximum-number-of-integers-to-choose-from-a-range-i.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     int maxCount(vector<int>& b, int n, int maxSum) {
-        unordered_map<int,int> m;
+        vector<int> m(1e4+1,0);
         for(auto it : b){
             m[it]++;
         }
         int cnt = 0;
         int sum = 0;
         for(int i = 1 ; i <= n ; i++){
-            if ( m.find(i) == m.end() && sum + i <= maxSum){
+            if ( m[i] == 0 && sum + i <= maxSum){
                 cnt++;
                 sum += i;
             }
