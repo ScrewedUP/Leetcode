b237619d3e957d4b325ba72bb2a504a2fbcecd90
ScrewedUP <43698882+ScrewedUP@users.noreply.github.com>
2024-12-22 23:08:47 +0530

    Time: 160 ms (5.72%) | Memory: 126.8 MB (7.14%) - LeetSync

--- 3373-maximum-prime-difference/maximum-prime-difference.cpp
+++ 3373-maximum-prime-difference/maximum-prime-difference.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
+    bool isPrime(int n) {
+        if (n <= 1) return false;
+        if (n == 2 || n == 3) return true;
+        if (n % 2 == 0 || n % 3 == 0) return false;
+
+        for (int i = 5; i * i <= n; i += 6) {
+            if (n % i == 0 || n % (i + 2) == 0) return false;
+        }
+        return true;
+    }
     int maximumPrimeDifference(vector<int>& nums) {
-        vector<int> primes = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97};
-        map<int,int> m;
-        for(auto it : primes){
-            m[it]++;
-        }
-        vector<int> indies;
+        set<int> s;
         for(int i = 0 ; i < nums.size() ; i++){
-            if ( m.find(nums[i]) != m.end()){
-                indies.push_back(i);
-            }
+            if ( isPrime(nums[i])) s.insert(i);
         }
-        return abs(indies.back() - indies[0]);
-        
+        return s.size() > 1 ? *s.rbegin() - *s.begin() : 0;
     }
 };
