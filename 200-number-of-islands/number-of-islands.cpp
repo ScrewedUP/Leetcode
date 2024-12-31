7fe69588d195896b12db1c168b7303a3915d10dc
ScrewedUP <43698882+ScrewedUP@users.noreply.github.com>
2024-12-31 11:53:12 +0530

    Time: 23 ms (85.37%) | Memory: 16.8 MB (58.09%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,26 +1,28 @@
 class Solution {
 public:
-    void dfs(int i,int j,int n,int m,vector<vector<bool>> &vis,vector<vector<char>>& grid){
-        if ( i >= n || j >= m || i < 0 || j < 0 || vis[i][j] || grid[i][j] == '0' ) return;
+    void dfs(int i,int j,vector<vector<bool>> &vis,vector<vector<char>> &grid,int n, int m){
+        if ( i >= n || j >= m || i < 0 || j < 0 || grid[i][j] == '0' || vis[i][j] ) return;
         vis[i][j] = true;
-        dfs(i+1,j,n,m,vis,grid);
-        dfs(i-1,j,n,m,vis,grid);
-        dfs(i,j+1,n,m,vis,grid);
-        dfs(i,j-1,n,m,vis,grid);
+        dfs(i+1,j,vis,grid,n,m);
+        dfs(i-1,j,vis,grid,n,m);
+        dfs(i,j+1,vis,grid,n,m);
+        dfs(i,j-1,vis,grid,n,m);
     }
     int numIslands(vector<vector<char>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
-        vector<vector<bool>> visited(n,vector<bool>(m,false));
-        int cnt = 0;
+        vector<vector<bool>> vis(n,vector<bool>(m,false));
+
+        int numOfIslands = 0;
+
         for(int i = 0 ; i < n ; i++){
-            for(int j = 0 ; j < m ; j++){
-                if ( grid[i][j] == '1' && !visited[i][j] ){
-                    cnt++;
-                    dfs(i,j,n,m,visited,grid);
+            for(int j = 0 ;j < m ; j++){
+                if ( grid[i][j] == '1' && vis[i][j] == false){  
+                    dfs(i,j,vis,grid,n,m);
+                    numOfIslands++;
                 }
             }
         }
-        return cnt;
+        return numOfIslands;
     }
 };
