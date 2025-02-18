dbc05bf4c52dbb99ba2e17cb20d3c7e5aa34a252
ScrewedUP <43698882+ScrewedUP@users.noreply.github.com>
2025-02-18 19:41:27 +0530

    Time: 2029 ms (5.07%) | Memory: 9.1 MB (8.72%) - LeetSync

---
+++ 2456-construct-smallest-number-from-di-string/construct-smallest-number-from-di-string.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    string ans = "99999999999";
+    bool check(string s,string p){
+        bool ok = true;
+        for(int i = 0 ; i < p.size() ; i++){
+            if ( p[i] == 'I'){
+                if ( s[i] - '0' > s[i+1] - '0'){
+                    ok = false;
+                    break;
+                }
+            }
+            else{
+                if ( s[i] - '0' < s[i+1] - '0'){
+                    ok = false;
+                    break;
+                }
+            }
+        }
+        return ok;
+    }
+    void solve(string s,int n,vector<int> &v,string &p){
+        if ( s.size() == n + 1){
+            if ( check(s,p) ){
+                ans = min(s,ans);
+            }
+            return;
+        }
+        for(int i = 1 ; i <= 9 ; i++){
+            if ( v[i] == 0){
+                s.push_back(i + '0');
+                v[i] = 1;
+                solve(s,n,v,p);
+                v[i] = 0;
+                s.pop_back();
+            }
+        }
+    }
+    string smallestNumber(string pattern) {
+        // Can try all possible patterns and check if it satisfy
+        int n = pattern.size();
+        vector<int> v(10,0);
+        solve("",n,v,pattern);
+        return ans;
+    }
+};
