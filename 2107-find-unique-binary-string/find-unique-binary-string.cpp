2703d033d8bf959b7bfe16b614ddfea84f7e8e72
ScrewedUP <43698882+ScrewedUP@users.noreply.github.com>
2025-02-20 08:00:17 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (94.35%) - LeetSync

--- 2107-find-unique-binary-string/find-unique-binary-string.cpp
+++ 2107-find-unique-binary-string/find-unique-binary-string.cpp
@@ -4,6 +4,8 @@ public:
         // Fliping the ith bit of the i-th string will
         // guarantee a different string from all the
         // elements in the array 
+        // ans will differ from every string in at least one position
+        // Cantor's diagonal argumenti
         string ans;
         for(int i = 0 ; i < nums.size() ; i++){
             if ( nums[i][i] == '0'){
