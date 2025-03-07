de456df18288206c9506643904e56a5a485d1c8a
ScrewedUP <43698882+ScrewedUP@users.noreply.github.com>
2025-03-07 16:26:29 +0530

    Time: 323 ms (32.22%), Space: 27 MB (27.22%) - LeetHub

---
+++ 2610-closest-prime-numbers-in-range/2610-closest-prime-numbers-in-range.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<bool> sieve(int n) {
+        vector<bool> primes(n + 1, true);
+        primes[0] = primes[1] = false;
+        for (int i = 2; i * i <= n; i++) {
+            if (primes[i]) {
+                for (int j = i * i; j <= n; j += i)
+                    primes[j] = false;
+            }
+        }
+        return primes;
+    }
+    vector<int> closestPrimes(int left, int right) {
+        vector<bool> primes = sieve(1000000);
+        vector<int> v;
+        for(int i = left ; i <= right ; i++){
+            if ( primes[i] == true){
+                v.push_back(i);
+            }
+        }
+        if ( v.size() < 2 ) return {-1,-1};
+        pair<int,int> p;
+        int diff = INT_MAX;
+        for(int i = 1 ; i < v.size() ; i++){
+            if ( (v[i] - v[i-1]) < diff){
+                p.first = v[i-1];
+                p.second = v[i];
+                diff = v[i] - v[i-1];
+            }
+        }
+        return {p.first,p.second};
+    }
+};
