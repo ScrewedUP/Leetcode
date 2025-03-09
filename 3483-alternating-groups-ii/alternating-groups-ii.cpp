c50974c442f6e37f010d00da0e9e0ed1d7f4a11f
ScrewedUP <43698882+ScrewedUP@users.noreply.github.com>
2025-03-09 17:08:46 +0530

    Time: 86 ms (11.51%) | Memory: 102.8 MB (43.10%) - LeetSync

--- 3483-alternating-groups-ii/alternating-groups-ii.cpp
+++ 3483-alternating-groups-ii/alternating-groups-ii.cpp
@@ -4,37 +4,19 @@ public:
         int cnt = 0;
         int curr = 1;
         int n = colors.size();
-        vector<int> temp(colors.begin(),colors.end());
-        for(auto it : colors){
-            temp.push_back(it);
-        }
-        if ( colors[0] == colors[colors.size() - 1]){
-            for(int i = 1 ; i < n ; i++){
-                if ( temp[i] == 1 - temp[i-1]){
-                    curr++;
-                    if ( curr == k ){
-                        cnt++;
-                        curr--;
-                    }
-                }
-                else{
-                    curr = 1;
-                }
+        
+        for(int i = 1 ; i < n + k - 1 ; i++){
+        if ( colors[i%n] == 1 - colors[(i-1)%n]){
+            curr++;
+            if ( curr == k ){
+                cnt++;
+                curr--;
             }
         }
         else{
-            for(int i = 1 ; i < n + k - 1 ; i++){
-            if ( temp[i] == 1 - temp[i-1]){
-                curr++;
-                if ( curr == k ){
-                    cnt++;
-                    curr--;
-                }
-            }
-            else{
-                curr = 1;
-            }
+            curr = 1;
         }
+        
         }
         
         return cnt;
