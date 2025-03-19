b455bf749b5af8c2b443de221aa1fbc4e5ecbc5f
ScrewedUP <43698882+ScrewedUP@users.noreply.github.com>
2025-03-19 21:16:09 +0530

    Time: 9 ms (40.49%) | Memory: 140.2 MB (15.99%) - LeetSync

--- 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/minimum-operations-to-make-binary-array-elements-equal-to-one-i.cpp
+++ 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/minimum-operations-to-make-binary-array-elements-equal-to-one-i.cpp
@@ -12,7 +12,7 @@ public:
             }
         }
         bool ok = true;
-        for(int i = 0 ; i < nums.size() ; i++){
+        for(int i = 0 ; i < n ; i++){
             if ( nums[i] == 0 ){
                 ok = false;
                 break;
