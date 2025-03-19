9d8462cccd4fe54037782e1beea8a54e3060ea29
ScrewedUP <43698882+ScrewedUP@users.noreply.github.com>
2025-03-19 21:18:12 +0530

    Time: 4 ms (81.56%), Space: 140 MB (47.55%) - LeetHub

--- 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i.cpp
+++ 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i.cpp
@@ -3,22 +3,15 @@ public:
     int minOperations(vector<int>& nums) {
         int n = nums.size();
         int cnt = 0;
-        for(int i = 0 ; i < n - 2 ; i++){
+        for(int i = 0 ; i < n ; i++){
             if ( nums[i] == 0){
+                if ( i + 1 >= n || i + 2 >= n) return -1;
                 nums[i] = 1 - nums[i];
                 nums[i+1] = 1 - nums[i+1];
                 nums[i+2] = 1 - nums[i+2];
                 cnt++;
             }
         }
-        bool ok = true;
-        for(int i = 0 ; i < n ; i++){
-            if ( nums[i] == 0 ){
-                ok = false;
-                break;
-            }
-        }
-        if ( ok ) return cnt;
-        return -1;
+        return cnt;
     }
 };
