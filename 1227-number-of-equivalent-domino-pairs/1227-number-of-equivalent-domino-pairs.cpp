7035a1366e909bc944b7f938ecb1d123190ac496
ScrewedUP <43698882+ScrewedUP@users.noreply.github.com>
2025-05-04 12:15:45 +0530

    Time: 12 ms (21.75%), Space: 28.4 MB (17.75%) - LeetHub

--- 1227-number-of-equivalent-domino-pairs/1227-number-of-equivalent-domino-pairs.cpp
+++ 1227-number-of-equivalent-domino-pairs/1227-number-of-equivalent-domino-pairs.cpp
@@ -1,25 +1,21 @@
 class Solution {
 public:
-    int numEquivDominoPairs(vector<vector<int>>& dominoes) {
-        vector<int> num(100);
-        int ret = 0;
-        for (auto& it : dominoes) {
-            int val = it[0] < it[1] ? it[0] * 10 + it[1] : it[1] * 10 + it[0];
-            /*
-                12 -> val = 12
-                21 -> val = 12
-                [[1,2],[1,2],[1,1],[1,2],[2,2]]
-
-                num[12]++;
-                num[12]++;
-                num[11]++;
-                num[12]++;
-                num[22]++;
-            */
-
-            ret += num[val];
-            num[val]++;
+    int numEquivDominoPairs(vector<vector<int>>& d) {
+        map<pair<int,int>,int> m;
+        int ans = 0;
+        for(auto it : d){
+            if ( it[0] > it[1]){
+                swap(it[0],it[1]);
+            }
+            pair<int,int> rev = {it[0],it[1]};
+            if ( m.find(rev) != m.end()){
+                ans += m[rev];
+            }
+            m[rev]++;
         }
-        return ret;
+        return ans;
+        
+        
     }
+
 };
