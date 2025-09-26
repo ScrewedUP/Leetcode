38960fb12a5286fed7d64979bc5e130791c8b14f
ScrewedUP <43698882+ScrewedUP@users.noreply.github.com>
2025-09-26 09:39:45 +0530

    Time: 187 ms (15.00%) | Memory: 16.5 MB (95.74%) - LeetSync

--- 611-valid-triangle-number/valid-triangle-number.cpp
+++ 611-valid-triangle-number/valid-triangle-number.cpp
@@ -1,22 +1,26 @@
 class Solution {
 public:
-    int triangleNumber(vector<int>& nums1) {
-        sort(nums1.begin(),nums1.end());
-        vector<int> nums;
-        for(auto it : nums1 ) if ( it != 0 ) nums.push_back(it);
+    int triangleNumber(vector<int>& nums) {
+        // for three sides a,b,c to be a triangle
+        // a + b > c 
+        // b + c > a
+        // a + c > b
+
+        // idea : from a sorted array i'll choose 2 number at index
+        // i and j , then i'll binary search the index 
+        sort(nums.begin(),nums.end());
+        int n = nums.size(); 
         int ans = 0;
-        int n = nums.size();
-        for( int i = 0 ; i < n ; i++ ){
-            for( int j = i + 1 ; j < n ; j++ ){
+        for(int i = 0 ; i < n ; i++){
+            for(int j = i + 1 ; j < n ; j++){
                 int sum = nums[i] + nums[j];
-                if ( sum <= 0 ) continue;
-                int idx = lower_bound(nums.begin(),nums.end(),sum) - nums.begin();
-                int ele = idx - j - 1;
-                ans += ele;
-                
+                int x = lower_bound(nums.begin(),nums.end(),sum) - nums.begin();
+                x--;
+                if ( x > j){
+                    ans += x - j;
+                }
             }
         }
         return ans;
-
     }
 };
