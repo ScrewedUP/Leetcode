0d3955a459d9bef170192746e807c2e89a3f1d29
ScrewedUP <43698882+ScrewedUP@users.noreply.github.com>
2025-10-02 09:15:58 +0530

    Time: 6 ms (5.85%) | Memory: 8.5 MB (44.88%) - LeetSync

--- 3336-water-bottles-ii/water-bottles-ii.cpp
+++ 3336-water-bottles-ii/water-bottles-ii.cpp
@@ -1,25 +1,14 @@
 class Solution {
 public:
     int maxBottlesDrunk(int numBottles, int numExchange) {
-        int ans = numBottles;
-        int exchanged = numBottles;
-        numBottles = 0;
-        while ( exchanged >= 0 ){
-            
-            if ( exchanged >= numExchange){
-                exchanged -= numExchange;
-                numBottles++;
-                numExchange++;
-            }
-            else if ( numBottles > 0 ){
-                ans+=numBottles;
-                exchanged += numBottles;
-                numBottles = 0;
-                
-            }
-            else break;
-            // cout << numBottles << " " << exchanged << " " << numExchange << endl;
+        int cnt = numBottles;
+        int empty = numBottles;
+        while ( empty >= numExchange ){
+            cnt++;
+            empty -= numExchange;
+            empty++;
+            numExchange++;
         }
-        return ans;
+        return cnt;
     }
 };
