1b74104ad59278e213fcae591109e34830548db7
ScrewedUP <43698882+ScrewedUP@users.noreply.github.com>
2025-10-02 09:55:06 +0530

    Time: 68 ms (57.35%) | Memory: 126.4 MB (79.89%) - LeetSync

--- 3430-count-days-without-meetings/count-days-without-meetings.cpp
+++ 3430-count-days-without-meetings/count-days-without-meetings.cpp
@@ -1,30 +1,20 @@
 class Solution {
 public:
-    int countDays(int days, vector<vector<int>>& meetings) {
-        sort(meetings.begin(),meetings.end());
-        long long cnt = 0;
-        int start = 1;
-        int curr = start;
-        int end = 0;
-        for(auto it : meetings){
-            end = max(end,it[1]);
-        }
-        for(int i = 0 ; i < meetings.size() ; i++){
-            if ( i == 0 ){
-                cnt += meetings[i][0] - start;
+    int countDays(int days, vector<vector<int>>& m) {
+        sort(m.begin(),m.end());
+        int curr = 0;
+        int cnt = 0;
+        for(int i = 0 ; i < m.size() ; i++){
+            if ( m[i][0] > curr ){
+                cnt += m[i][0] - curr - 1;
                 
             }
-            else{
-                if ( meetings[i][0] > meetings[i-1][1] && meetings[i][0] > curr){
-                    cnt += meetings[i][0] - max(meetings[i-1][1],curr) - 1;
-                }
-            }
-            start = min(start,meetings[i][0]);
-                curr = max(curr,meetings[i][1]);
+            curr = max(curr,m[i][1]);
         }
-        if ( days - end > 0 ){
-            cnt += days - end;
+        if ( curr < days){
+            cnt += days - curr;
         }
         return cnt;
+        
     }
 };
