edecf75f6b04a5318e4243702ec5d11a77311462
ScrewedUP <43698882+ScrewedUP@users.noreply.github.com>
2025-10-04 17:06:58 +0530

    Time: 3 ms (35.32%) | Memory: 63 MB (12.56%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.cpp
+++ 11-container-with-most-water/container-with-most-water.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
     int maxArea(vector<int>& h) {
-        int maxi = INT_MIN;
-        int i = 0;
+        int ans = INT_MIN;
+        int i = 0 ;
         int j = h.size() - 1;
-        while ( i < j ){
-            maxi = max(maxi,min(h[i],h[j])*(j-i));
-            if ( h[i] <= h[j] ){
+        while ( i < j){
+            int p = min(h[i],h[j]);
+            ans = max(ans,p*(j-i));
+            if ( h[i] < h[j]){
                 i++;
             }
             else j--;
         }
-        return maxi;
+        return ans;
     }
 };
