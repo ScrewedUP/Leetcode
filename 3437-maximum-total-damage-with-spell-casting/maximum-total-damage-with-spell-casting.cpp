53e91a094c4c48721ac21bd55ba0c2b9cc4316a1
ScrewedUP <43698882+ScrewedUP@users.noreply.github.com>
2025-10-11 11:01:35 +0530

    Time: 843 ms (5.20%) | Memory: 289.9 MB (5.20%) - LeetSync

--- 3437-maximum-total-damage-with-spell-casting/maximum-total-damage-with-spell-casting.cpp
+++ 3437-maximum-total-damage-with-spell-casting/maximum-total-damage-with-spell-casting.cpp
@@ -1,33 +1,27 @@
 class Solution {
 public:
-    long long t[100001];
-    long long solve(int idx,vector<int> &ans,map<int,int> &m){
-        if ( idx >= ans.size() ) return 0;
-        if ( t[idx] != -1 ) return t[idx];
-
-        long long take = ((long long)m[ans[idx]] * (long long)ans[idx]);
-        if ( idx + 1 < ans.size() && ans[idx] + 2 >= ans[idx+1] ){
-            long long curr = ans[idx];
-            int i = idx + 1;
-            while ( i < ans.size() && ans[i] <= curr + 2){
-                i++;
-            }
-            take += solve(i,ans,m);
-        }
-        else if ( idx + 1 < ans.size() && ans[idx] + 2 < ans[idx+1]){
-            take += solve(idx+1,ans,m);
-        }
-        long long notTake = solve(idx+1,ans,m);
-        return t[idx] = max(take,notTake);
-    }
-    long long maximumTotalDamage(vector<int>& v) {
-        memset(t,-1,sizeof(t));
-        sort(v.begin(),v.end());
-        map<int,int> m;
-        for(auto it : v) m[it]++;
-        vector<int> ans;
-        for(auto it : m ) ans.push_back(it.first);
-        return solve(0,ans,m);
+    long long maximumTotalDamage(vector<int>& power) {
         
+        map<long long,long long> m;
+        for(auto it : power){
+            m[it]++;
+        }
+        set<long long> s(power.begin(),power.end());
+        vector<long long> v(s.begin(),s.end());
+        vector<long long> t(v.size(),0);
+        for(int i = 0 ; i < v.size() ; i++){
+
+            for(int j = 1 ; j < 4 ; j++){
+                if ( i - j >= 0 && v[i] - v[i-j] > 2){
+                    t[i] = max(t[i],t[i-j]);
+                }
+            }
+            t[i] += m[v[i]]*v[i];
+            if ( i - 1 >= 0){
+                t[i] = max(t[i],t[i-1]);
+            }
+        }
+        long long ans = *max_element(t.begin(),t.end());
+        return ans;
     }
 };
