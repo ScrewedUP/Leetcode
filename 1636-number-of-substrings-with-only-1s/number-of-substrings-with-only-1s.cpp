ee365c6dae603f28ac0924e9212d51812f88143a
ScrewedUP <43698882+ScrewedUP@users.noreply.github.com>
2025-11-16 17:47:29 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (39.33%) - LeetSync

--- 1636-number-of-substrings-with-only-1s/number-of-substrings-with-only-1s.cpp
+++ 1636-number-of-substrings-with-only-1s/number-of-substrings-with-only-1s.cpp
@@ -1,21 +1,26 @@
 class Solution {
 public:
-    const int m = 1e9 +7;
     int numSub(string s) {
-        int ans = 0;
+        int M = 1e9 + 7;
+        long long ans = 0;
         long long cnt = 0;
-        for(int i = 0 ; i < s.size(); i++){
+        for(int i = 0 ; i < s.size() ; i++){
             if ( s[i] == '1'){
                 cnt++;
             }
             else{
-
-                ans = (ans + cnt*(cnt+1)/2)%m;
+                if ( cnt > 0 ){
+                    ans += cnt*(cnt+1)/2;
+                    ans %= M;
+                }
                 cnt = 0;
             }
+
         }
         if ( cnt > 0 ){
-            ans = (ans + cnt*(cnt+1)/2)%m;
+            ans += cnt*(cnt+1)/2;
+            ans %= M;
+            
         }
         return ans;
     }
