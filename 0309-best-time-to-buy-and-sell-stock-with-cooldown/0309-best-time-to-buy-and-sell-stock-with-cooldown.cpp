4afaf470f922982968b3d35a47dd3655d7c72ffc
ScrewedUP <43698882+ScrewedUP@users.noreply.github.com>
2025-11-27 20:36:10 +0530

    Time: 0 ms (100%), Space: 15 MB (91.54%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,16 +1,19 @@
 class Solution {
 public:
+    int t[5002][3];
     int maxProfit(vector<int>& prices) {
+        memset(t,0,sizeof(t));
         int n = prices.size();
-        vector<vector<int>> dp(n + 2, vector<int>(2, 0));
-        for (int i = n - 1; i >= 0; i--) {
-            int skip0 = dp[i + 1][0];
-            int buy = -prices[i] + dp[i + 1][1];
-            dp[i][0] = max(skip0, buy);
-            int skip1 = dp[i + 1][1];
-            int sell = prices[i] + dp[i + 2][0];
-            dp[i][1] = max(skip1, sell);
+        for(int i = n - 1 ; i >= 0 ; i--){
+            for(int w = 0 ; w <= 1 ; w++){
+                if ( w == 0){
+                    t[i][w] = max(t[i+1][w] , -1*prices[i] + t[i+1][1]);
+                }
+                else{
+                    t[i][w] = max(t[i+1][w] , prices[i] + t[i+2][0]);
+                }
+            }
         }
-        return dp[0][0];
+        return t[0][0];
     }
 };
