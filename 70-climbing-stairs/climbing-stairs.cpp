a4cff068fefe947d013bef1c7c0771101cb25610
ScrewedUP <43698882+ScrewedUP@users.noreply.github.com>
2025-11-27 22:11:59 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (98.29%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -3,11 +3,10 @@ public:
     int t[50];
     int climbStairs(int n) {
         memset(t,0,sizeof(t));
-        t[0] = 1;
-        t[1] = 1;
-        for(int i = 2 ; i <= n ; i++){
-            t[i] = t[i-1] + t[i-2];
+        t[n] = 1;
+        for(int i = n - 1; i >= 0 ; i--){
+            t[i] = t[i+1] + t[i+2];
         }
-        return t[n];
+        return t[0];
     }
 };
