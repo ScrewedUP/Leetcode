a3fe99ab64389d515eb14f2d62876f033c6d30f2
ScrewedUP <43698882+ScrewedUP@users.noreply.github.com>
2025-11-27 22:12:28 +0530

    Time: 0 ms (100%), Space: 8.5 MB (48.31%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,8 +1,7 @@
 class Solution {
 public:
-    int t[50];
     int climbStairs(int n) {
-        memset(t,0,sizeof(t));
+        vector<int> t(n+2,0);
         t[n] = 1;
         for(int i = n - 1; i >= 0 ; i--){
             t[i] = t[i+1] + t[i+2];
