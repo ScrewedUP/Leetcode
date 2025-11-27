ec774f5dbc5e954f48a93b59f62a307ea9e158fd
ScrewedUP <43698882+ScrewedUP@users.noreply.github.com>
2025-11-27 22:08:37 +0530

    Time: 0 ms (100%), Space: 7.8 MB (70.85%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,14 +1,6 @@
 class Solution {
 public:
     int t[50];
-    int f(int i,int n){
-        if ( i > n ) return 0;
-        if ( i == n ) return 1;
-        if ( t[i] != -1 ) return t[i];
-        int one = f(i+1,n);
-        int two = f(i+2,n);
-        return t[i] = one + two;
-    }
     int climbStairs(int n) {
         memset(t,0,sizeof(t));
         t[0] = 1;
