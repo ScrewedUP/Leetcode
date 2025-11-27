d2d3d7591408e6f62f5c700da3bf601c802da8a8
ScrewedUP <43698882+ScrewedUP@users.noreply.github.com>
2025-11-27 22:08:27 +0530

    Time: 0 ms (100%), Space: 7.9 MB (70.85%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -10,7 +10,12 @@ public:
         return t[i] = one + two;
     }
     int climbStairs(int n) {
-        memset(t,-1,sizeof(t));
-        return f(0,n);
+        memset(t,0,sizeof(t));
+        t[0] = 1;
+        t[1] = 1;
+        for(int i = 2 ; i <= n ; i++){
+            t[i] = t[i-1] + t[i-2];
+        }
+        return t[n];
     }
 };
