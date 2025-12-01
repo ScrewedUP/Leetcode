38ae0f97600efeefae465b53ead1befea274d3c5
ScrewedUP <43698882+ScrewedUP@users.noreply.github.com>
2025-12-01 14:33:01 +0530

    Time: 0 ms (100%), Space: 11.3 MB (81.35%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,90 +1,28 @@
 class Solution {
 public:
-    bool check(vector<int> &v,vector<int> &t,char l){
-        if ( l >= 'a'){
-            v[l - 'a']--;
-        }
-        else{
-            v[l - 'A' + 26]--;
-        }
-        for(int i = 0 ; i < t.size() ; i++){
-            if ( t[i] > 0 ){
-                if ( v[i] < t[i]){
-                    if ( l >= 'a'){
-                        v[l - 'a']++;
-                    }
-                    else{
-                        v[l - 'A' + 26]++;
-                    }
-                    return false;
-                }
-            }
-        }
-        if ( l >= 'a'){
-            v[l - 'a']++;
-        }
-        else{
-            v[l - 'A' + 26]++;
-        }
-        // cout << "here1" << endl;
-        return true;
-    }
-    bool ok(vector<int> &v,vector<int> &t){
-        for(int i = 0 ; i < t.size() ; i++){
-            if ( t[i] > 0 ){
-                if ( v[i] < t[i]) return false;
-            }
-        }
-        return true;
-    }
     string minWindow(string s, string t) {
-        vector<int> tar(53,0);
-        for(int i = 0 ; i < t.size() ; i++){
-            if ( t[i] >= 'a'){
-                tar[t[i] - 'a']++;
-            }
-            else{
-                tar[t[i] - 'A' + 26]++;
-            }
-            
-        }
-        vector<int> v(53,0);
         int n = s.size();
-        int st = -1;
-        int en = -1;
-        int len = INT_MAX;
+        vector<int> v(128, 0);
+        for (char c : t) v[c]++;
+        
         int l = 0;
-        for(int i = 0 ; i < n ; i++){
-            if ( s[i] >= 'a'){
-                v[s[i] - 'a']++;
-            }
-            else{
-                v[s[i] - 'A' + 26]++;
-            }
-            if ( ok(v,tar) ){
-                // if ( i == 10 ) cout << "here";
-                while(check(v,tar,s[l])){
-                    if ( s[l] >= 'a'){
-                        v[s[l] - 'a']--;
-                    }
-                    else{
-                        v[s[l] - 'A' + 26]--;
-                    }
-                    l++;
-                }    
-            }
-            if ( ok(v,tar)){
-                if ( i - l + 1 < len){
-                    st = l;
-                    en = i;
+        int st = 0;
+        int len = INT_MAX;
+        int count = t.size();
+        
+        for (int i = 0; i < n; i++) {
+            if (v[s[i]]-- > 0) count--;
+            
+            while (count == 0) {
+                if (i - l + 1 < len) {
                     len = i - l + 1;
+                    st = l;
                 }
-                
+                if (v[s[l]]++ == 0) count++;
+                l++;
             }
         }
-        // cout << st << " " << en;
-        if ( st == -1 ) return "";
-        return s.substr(st,en-st+1);
-        // return s.substr(st,en-st+1);
+        
+        return len == INT_MAX ? "" : s.substr(st, len);
     }
 };
