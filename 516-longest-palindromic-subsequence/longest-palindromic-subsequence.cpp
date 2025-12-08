edb017e3477cc0dd2b0e8de461eecb05edd265c3
ScrewedUP <43698882+ScrewedUP@users.noreply.github.com>
2025-12-08 22:00:28 +0530

    Time: 26 ms (96.24%) | Memory: 13 MB (87.42%) - LeetSync

---
+++ 516-longest-palindromic-subsequence/longest-palindromic-subsequence.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int dp[1001][1001];
+    int solve(int i ,int j ,string &s){
+        if ( i == j ) return 1;
+        if ( i > j ) return 0;
+        if ( dp[i][j] != -1 ) return dp[i][j];
+        int ans = 0;
+        if ( s[i] == s[j] ){
+            ans = max(ans,2 + solve(i+1,j-1,s));
+        }
+        else{
+            ans = max(ans,solve(i+1,j,s));
+            ans = max(ans,solve(i,j-1,s));
+        }
+        return dp[i][j] = ans;
+    }
+
+    int longestPalindromeSubseq(string s) {
+        memset(dp,-1,sizeof(dp));
+        return solve(0,s.size()-1,s);
+    }
+};
