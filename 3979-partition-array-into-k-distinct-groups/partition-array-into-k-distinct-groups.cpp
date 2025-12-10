928879344fbf41035b4fa7e8130f3dd4d7b5edfd
ScrewedUP <43698882+ScrewedUP@users.noreply.github.com>
2025-12-11 00:10:59 +0530

    Time: 51 ms (67.54%) | Memory: 285.7 MB (59.43%) - LeetSync

--- 3979-partition-array-into-k-distinct-groups/partition-array-into-k-distinct-groups.cpp
+++ 3979-partition-array-into-k-distinct-groups/partition-array-into-k-distinct-groups.cpp
@@ -1,13 +1,16 @@
 class Solution {
 public:
+    /*
+        k == 2;
+        1,1,2,2,3,3
+        [1_ 2_] [1_ 3_] [2_ 3_] 
+    */
     bool partitionArray(vector<int>& nums, int k) {
+        if ( nums.size() % k != 0 ) return false;
         int n = nums.size();
-        if ( n % k != 0 ) return false;
-        map<int,int> m;
-        for(auto it : nums){
-            m[it]++;
-        }
         int x = n/k;
+        unordered_map<int,int> m;
+        for(auto it : nums) m[it]++;
         for(auto it : m){
             if ( it.second > x ) return false;
         }
