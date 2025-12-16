d46866d5f6118c5ab2be8282c5f054e8bfa47a55
ScrewedUP <43698882+ScrewedUP@users.noreply.github.com>
2025-12-16 19:07:14 +0530

    Time: 115 ms (38.91%) | Memory: 124.5 MB (60.36%) - LeetSync

--- 2656-count-the-number-of-beautiful-subarrays/count-the-number-of-beautiful-subarrays.cpp
+++ 2656-count-the-number-of-beautiful-subarrays/count-the-number-of-beautiful-subarrays.cpp
@@ -1,8 +1,7 @@
 class Solution {
 public:
     long long beautifulSubarrays(vector<int>& nums) {
-        int target = 0;
-        map<int,int> m;
+        unordered_map<int,int> m;
         m[0] = 1;
         int n = nums.size();
         long long x = 0;
@@ -10,13 +9,11 @@ public:
 
         for(int i = 0 ; i < n ; i++){
             x ^= nums[i];
-            int t = x^target;
-
-            if ( m.find(t) != m.end()){
-                ans += m[t];
+            if ( m.find(x) != m.end()){
+                ans += m[x];
             }
 
-            m[t]++;
+            m[x]++;
         }
         return ans;
 
