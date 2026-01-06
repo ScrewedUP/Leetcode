ee93c0d2bea7711ddf982f5ef3055953405b7504
ScrewedUP <43698882+ScrewedUP@users.noreply.github.com>
2026-01-06 19:16:57 +0530

    Time: 2 ms (90.79%) | Memory: 8.7 MB (98.54%) - LeetSync

---
+++ 38-count-and-say/count-and-say.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    string countAndSay(int n) {
+        string curr = "1";
+        for(int i = 1 ; i < n ; i++){
+            int cnt = 1;
+            char c = curr[0];
+            string temp;
+            if ( curr.size() == 1){
+                temp = "11";
+            }
+            else{
+                for(int j = 1 ; j < curr.size() ; j++){
+                    if ( curr[j] == c ){
+                        cnt++;
+                        if ( j == curr.size() - 1){
+                            temp += ( cnt + '0');
+                            temp += c;
+                        }
+                    }
+                    else{
+                        temp += ( cnt + '0');
+                        temp += c;
+                        cnt = 1;
+                        c = curr[j];
+                        if ( j == curr.size() - 1){
+                            temp += ( cnt + '0');
+                            temp += c;
+                        }
+                    }
+                }
+            }
+            curr = temp;
+        }
+        return curr;
+    }
+};
