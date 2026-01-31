c5337f8c564a5610e8cf7c930db3334475015afd
ScrewedUP <43698882+ScrewedUP@users.noreply.github.com>
2026-01-31 18:01:01 +0530

    Time: 0 ms (100%), Space: 19 MB (46.89%) - LeetHub

---
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int dfs(TreeNode* root){
+        if ( !root ) return 0;
+
+        int x = 1 + max(dfs(root->left),dfs(root->right));
+
+        return x;
+    }
+    int maxDepth(TreeNode* root) {
+        if ( !root ) return 0;
+        int x = dfs(root);
+        return x;
+    }
+};
