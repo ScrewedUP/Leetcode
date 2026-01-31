2b87cfd2db8c6609d3ef3025024ff5d86bf40a35
ScrewedUP <43698882+ScrewedUP@users.noreply.github.com>
2026-01-31 12:28:11 +0530

    Time: 0 ms (100.00%) | Memory: 10.9 MB (65.29%) - LeetSync

---
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        // inorder-> left root right
+
+        if ( !root ) return {};
+
+        vector<int> ans;
+
+        stack<TreeNode*> s;
+        TreeNode* node = root;
+        // will go left till possible when null print and go right
+
+        while(1){
+            if (node){
+                s.push(node);
+                node = node->left;
+            }
+            else{
+                if ( s.empty()) break;
+
+                // at this point we have been to the leftmost
+                // node , so pop the top element of stack which will
+                // be the left most element
+
+                node = s.top();
+                s.pop();
+                ans.push_back(node->val);
+                node = node->right;
+            }
+        }
+        return ans;
+    }
+};
