f6b31730c2dfca4d729772763a7facb8f994297c
ScrewedUP <43698882+ScrewedUP@users.noreply.github.com>
2026-02-21 12:46:43 +0530

    Time: 3 ms (72.17%), Space: 8.1 MB (35.12%) - LeetHub

---
+++ 0762-prime-number-of-set-bits-in-binary-representation/0762-prime-number-of-set-bits-in-binary-representation.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int countPrimeSetBits(int left, int right) {
+        int cnt = 0;
+        set<int> s{2,3,5,7,11,13,17,19,23,29};
+        for(int i = left ; i <= right ; i++){
+            int x = __builtin_popcount(i);
+            if ( s.find(x) != s.end()){
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+};
