7b979db7ed55e0c3864d8f9ca0df022912192733
ScrewedUP <43698882+ScrewedUP@users.noreply.github.com>
2026-02-21 19:51:11 +0530

    Time: 19 ms (55.96%), Space: 34.8 MB (63.93%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -1,35 +1,32 @@
-#define INF 1e9
 class Solution {
 public:
     int d[4][2] = {{1,0},{-1,0},{0,1},{0,-1}};
+
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-       int n = mat.size();
-       int m = mat[0].size();
-       queue<pair<int,int>> q;
-       vector<vector<int>> dis(n,vector<int>(m,INF)); 
-       for(int i = 0 ; i < n ; i++){
-            for(int j = 0 ; j < m ; j++){
-                if ( mat[i][j] == 0){
-                    q.push({i,j});
+        int n = mat.size(), m = mat[0].size();
+        queue<pair<int,int>> q;
+        vector<vector<int>> dis(n, vector<int>(m, INT_MAX));
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (mat[i][j] == 0) {
                     dis[i][j] = 0;
+                    q.push({i,j});
                 }
             }
         }
 
-        while(!q.empty()){
-            int sz = q.size();
-            while(sz--){
-                auto f = q.front(); q.pop();
-                for(int i = 0 ; i < 4 ; i++){
-                    int dx = f.first + d[i][0];
-                    int dy = f.second + d[i][1];
-                    auto ok = [&](int a,int b){
-                        return a >= 0 && b >= 0 && a < n && b < m;
-                    };
-                    if ( ok(dx,dy) && mat[dx][dy] == 1 && dis[dx][dy] == INF){
-                        dis[dx][dy] = min(dis[dx][dy],dis[f.first][f.second] + 1);
-                        q.push({dx,dy});
-                    }
+        while (!q.empty()) {
+            auto [x,y] = q.front(); q.pop();
+
+            for (auto &k : d) {
+                int dx = x + k[0], dy = y + k[1];
+
+                if (dx >= 0 && dx < n && dy >= 0 && dy < m &&
+                    dis[dx][dy] > dis[x][y] + 1)
+                {
+                    dis[dx][dy] = dis[x][y] + 1;
+                    q.push({dx,dy});
                 }
             }
         }
