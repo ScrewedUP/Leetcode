f1ed29b6d2eb6bbc1c8a6ae246ebf870a55ae4c4
ScrewedUP <43698882+ScrewedUP@users.noreply.github.com>
2026-02-21 19:25:56 +0530

    Time: 4 ms (15.84%), Space: 16.6 MB (87.06%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -1,45 +1,42 @@
 class Solution {
 public:
     int d[4][2] = {{1,0},{-1,0},{0,1},{0,-1}};
+
     int orangesRotting(vector<vector<int>>& grid) {
-        int n = grid.size();
-        int m = grid[0].size();
+        int n = grid.size(), m = grid[0].size();
         queue<pair<int,int>> q;
-        for(int i = 0 ; i < n ; i++){
-            for(int j = 0 ; j < m ; j++){
-                if ( grid[i][j] == 2 ){
-                    q.push({i,j});
-                }
+        int fresh = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == 2) q.push({i,j});
+                else if (grid[i][j] == 1) fresh++;
             }
         }
-        int cnt = 0;
-        while(!q.empty()){
-            int x = q.size();
-            bool ok = false;
-            for(int j = 0 ; j < x ; j++){
-                auto f = q.front(); q.pop();
-                
-                for(int i = 0 ; i < 4 ; i++){
-                    int dx = f.first + d[i][0];
-                    int dy = f.second + d[i][1];
 
-                    if ( dx >= 0 && dx < n && dy >= 0 && dy < m && grid[dx][dy] == 1){
-                        ok = true;
+        int time = 0;
+
+        while (!q.empty() && fresh > 0) {
+            int sz = q.size();
+
+            while (sz--) {
+                auto [x,y] = q.front(); q.pop();
+
+                for (auto &k : d) {
+                    int dx = x + k[0], dy = y + k[1];
+
+                    if (dx >= 0 && dx < n && dy >= 0 && dy < m &&
+                        grid[dx][dy] == 1)
+                    {
                         grid[dx][dy] = 2;
                         q.push({dx,dy});
+                        fresh--;
                     }
                 }
             }
-            if ( ok ) cnt++;
-            
+            time++;
         }
-        for(int i = 0 ;i < n ; i++ ){
-            for(int j = 0 ; j < m ; j++){
-                if ( grid[i][j] == 1 ){
-                    return -1;
-                }
-            }
-        }
-        return cnt;
+
+        return fresh == 0 ? time : -1;
     }
 };
