a5a0c44f17e1bebb82ca709a57a56eae0d320b9b
ScrewedUP <43698882+ScrewedUP@users.noreply.github.com>
2026-02-24 21:04:09 +0530

    Time: 0 ms (100%), Space: 17.3 MB (53.22%) - LeetHub

---
+++ 1022-sum-of-root-to-leaf-binary-numbers/1022-sum-of-root-to-leaf-binary-numbers.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int ans = 0;
+
+    void dfs(TreeNode* root, int cur) {
+        if (!root) return;
+
+        cur = cur * 2 + root->val;
+
+        if (!root->left && !root->right) {
+            ans += cur;
+            return;
+        }
+
+        dfs(root->left, cur);
+        dfs(root->right, cur);
+    }
+
+    int sumRootToLeaf(TreeNode* root) {
+        dfs(root, 0);
+        return ans;
+    }
+};
