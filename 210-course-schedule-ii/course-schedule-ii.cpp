f2ea4948748a46f3d5deec4a34485715ce005425
ScrewedUP <43698882+ScrewedUP@users.noreply.github.com>
2026-03-14 16:20:41 +0530

    Time: 3 ms (72.38%) | Memory: 19 MB (22.92%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.cpp
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -2,36 +2,31 @@ class Solution {
 public:
     vector<int> findOrder(int n, vector<vector<int>>& p) {
         vector<vector<int>> g(n);
-        vector<int> in(n,0);
-
-        for(auto e : p){
-            g[e[0]].push_back(e[1]);
-            in[e[1]]++;
+        vector<int> ind(n,0);
+        for(auto it : p){
+            g[it[0]].push_back(it[1]);
+            ind[it[1]]++;
         }
 
         queue<int> q;
-        for(int i = 0 ; i < n; i++){
-            if ( in[i] == 0){
+        for(int i = 0 ;i < n ; i++){
+            if ( ind[i] == 0){
                 q.push(i);
             }
         }
-
         vector<int> topo;
-
+        int cnt = 0;
         while(!q.empty()){
             int f = q.front(); q.pop();
-
             topo.push_back(f);
-
-            for(auto v : g[f]){
-                in[v]--;
-                if ( in[v] == 0){
-                    q.push(v);
-                }
+            cnt++;
+            for(auto  v : g[f]){
+                ind[v]--;
+                if ( ind[v] == 0 ) q.push(v);
             }
         }
-        if ( topo.size() != n ) return {};
         reverse(topo.begin(),topo.end());
-        return topo;
+        if ( cnt == n ) return topo;
+        return {};
     }
 };
