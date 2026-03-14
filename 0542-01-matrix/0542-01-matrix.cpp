aa213500508c7591f76618961a9b724e79fb0d38
ScrewedUP <43698882+ScrewedUP@users.noreply.github.com>
2026-03-14 15:34:02 +0530

    Time: 1 ms (99.03%), Space: 34.9 MB (59.17%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -1,35 +1,43 @@
 class Solution {
 public:
-    int d[4][2] = {{1,0},{-1,0},{0,1},{0,-1}};
-
+    int dx[4] = {-1,0,1,0};
+    int dy[4] = {0,1,0,-1};
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-        int n = mat.size(), m = mat[0].size();
         queue<pair<int,int>> q;
-        vector<vector<int>> dis(n, vector<int>(m, INT_MAX));
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (mat[i][j] == 0) {
-                    dis[i][j] = 0;
+        int n = mat.size();
+        int m = mat[0].size();
+        vector<vector<int>> ans(n,vector<int>(m,INT_MAX));
+        for(int i = 0 ; i < n; i++){
+            for(int j = 0 ; j < m ; j++){
+                if ( mat[i][j] == 0){
+                    ans[i][j] = 0;
                     q.push({i,j});
                 }
             }
         }
+        int time = 0;
+        while ( !q.empty() ){
+            int x = q.size();
+            time++;
+            while(x--){
+                auto t = q.front();
+                q.pop();
+                for(int i = 0 ; i < 4 ; i++){
+                    int d_x = t.first + dx[i];
+                    int d_y = t.second + dy[i];
 
-        while (!q.empty()) {
-            auto [x,y] = q.front(); q.pop();
-
-            for (auto &k : d) {
-                int dx = x + k[0], dy = y + k[1];
-
-                if (dx >= 0 && dx < n && dy >= 0 && dy < m &&
-                    dis[dx][dy] > dis[x][y] + 1)
-                {
-                    dis[dx][dy] = dis[x][y] + 1;
-                    q.push({dx,dy});
+                    if ( d_x < n && d_x >= 0 && d_y < m && d_y >=0 ){
+                        if ( mat[d_x][d_y] == 1){
+                            int ct = time;
+                            if ( ans[d_x][d_y] == INT_MAX ){
+                                ans[d_x][d_y] = ct;
+                                q.push({d_x,d_y});
+                            }
+                        }
+                    }
                 }
             }
         }
-        return dis;
+        return ans;
     }
 };
