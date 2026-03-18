45e5c93b8555f40d62532f46cd771c83c5fac01a
ScrewedUP <43698882+ScrewedUP@users.noreply.github.com>
2026-03-18 22:28:39 +0530

    Time: 1 ms (22.68%), Space: 9 MB (37.6%) - LeetHub

---
+++ 3174-clear-digits/3174-clear-digits.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string clearDigits(string s) {
+        stack<char> st;
+        for(auto it : s){
+            if ( it >= '0' && it <= '9'){
+                if ( !st.empty()) st.pop();
+            }
+            else st.push(it);
+        }  
+        string ans = "";
+        while(!st.empty()){
+            ans += st.top();st.pop();
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
