82f99df568ce82b77d1cbc18a07130129df58198
ScrewedUP <43698882+ScrewedUP@users.noreply.github.com>
2026-03-21 10:14:00 +0530

    Time: 0 ms (100.00%) | Memory: 33.7 MB (93.98%) - LeetSync

--- 3973-flip-square-submatrix-vertically/flip-square-submatrix-vertically.cpp
+++ 3973-flip-square-submatrix-vertically/flip-square-submatrix-vertically.cpp
@@ -10,25 +10,11 @@ public:
 
             (x,y+k)     (x+k,y+k)
 
-            k = 7;
-            0,6
-            1,5
-            2,4
-            k - i - offset
-            [14,3,18,16]
-            [2,14,11,20]
-            [19,19,4,15]
-            [11,15,18,6]
-            [x,y,z,t]
-            [x,y,z,t]
-            [x,y,z,t]
     */
     vector<vector<int>> reverseSubmatrix(vector<vector<int>>& grid, int x, int y, int k) {
         int offset = 0;
         for(int i = x ; i < (k/2 + x) ; i++){
-            // cout << i << " ";
             for(int j = y ; j < y+k ; j++){
-                cout << "|" << i << ":" << j << "|" << "<->" << i+k-1-offset << ":" << j << endl;
                 swap(grid[i][j],grid[x + k - 1 - offset][j]);
             }
             offset++;
