5fce7fdd8130a35a791b7b75aa689bd1e062da65
ScrewedUP <43698882+ScrewedUP@users.noreply.github.com>
2026-03-25 18:59:49 +0530

    Time: 70 ms (18.51%) | Memory: 165.8 MB (65.99%) - LeetSync

--- 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.cpp
+++ 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.cpp
@@ -2,12 +2,7 @@ class Solution {
 public:
     bool uniformArray(vector<int>& v) {
         sort(v.begin(),v.end());
-        int x = v[0];
-
-        if ( x % 2 == 1 ){
-            return true;
-        }
-
+        if ( v[0] & 1 ) return true;
         for(int i = 0 ; i < v.size() ; i++){
             if ( v[i] & 1 ) return false;
         }
