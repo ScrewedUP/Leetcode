db01bfe959a0350093d483f3868dc9c3c89d9d52
ScrewedUP <43698882+ScrewedUP@users.noreply.github.com>
2026-05-10 17:32:38 +0530

    Time: 19 ms (62.42%) | Memory: 67.3 MB (97.83%) - LeetSync

--- 2855-maximum-number-of-jumps-to-reach-the-last-index/maximum-number-of-jumps-to-reach-the-last-index.cpp
+++ 2855-maximum-number-of-jumps-to-reach-the-last-index/maximum-number-of-jumps-to-reach-the-last-index.cpp
@@ -1,16 +1,21 @@
 class Solution {
 public:
-    int maximumJumps(vector<int>& a, int t) {
-        int n = a.size();
-        const int NEG = -100000000; 
-        vector<int> dp(n, NEG);
-        dp[n-1] = 0;
-        for (int i = n-2; i >= 0; i--) {
-            for (int j = i+1; j < n; j++) {
-                int d = a[j] - a[i];
-                if (d <= t && d >= -t) dp[i] = max(dp[i], 1 + dp[j]);
+    int dp[1001];
+    int solve(int i,vector<int> &nums,int t ){
+        if ( i >= nums.size()) return -1e5;
+        if ( i == nums.size()-1) return 0;
+        if ( dp[i] != -1 ) return dp[i];
+        int ans = -1e6;
+        for(int j = i + 1 ; j < nums.size() ; j++){
+            if ( nums[j] - nums[i] <= t && nums[j] - nums[i] >= -t){
+                ans = max(ans,1 + solve(j,nums,t));
             }
         }
-        return dp[0] < 0 ? -1 : dp[0];
+        return dp[i] = ans;
+    }
+    int maximumJumps(vector<int>& nums, int target) {
+        memset(dp,-1,sizeof(dp));
+        int x = solve(0,nums,target); 
+        return x >= 0 ? solve(0,nums,target) : -1;
     }
 };
