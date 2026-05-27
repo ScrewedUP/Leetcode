1a223b0a8d69516b1a793ea6dc2dc28a6e64e1d3
ScrewedUP <43698882+ScrewedUP@users.noreply.github.com>
2026-05-27 08:40:36 +0530

    Time: 84 ms (32.86%), Space: 25.7 MB (16.01%) - LeetHub

---
+++ 3121-count-the-number-of-special-characters-ii/3121-count-the-number-of-special-characters-ii.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int numberOfSpecialChars(string word) {
+        unordered_map<char,int> l,u;
+
+        for(int i = 0 ; i < word.size() ; i++){
+            if ( word[i] >= 'a' && word[i] <= 'z'){
+                l[word[i]] = i;
+            }
+            else{
+                if ( u.find(word[i]) == u.end()) u[word[i]] = i;
+            }
+        }
+        int cnt = 0;
+        for(auto it : l){
+            char c = toupper(it.first);
+            if ( u.find(c) != u.end()){
+                if ( it.second < u[c]) cnt++;
+            }
+        }
+        return cnt;
+    }
+};
