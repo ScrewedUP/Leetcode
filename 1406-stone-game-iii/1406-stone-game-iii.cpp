b89ef3fbfda95a277e8865d57286b23931f7f8c4
ScrewedUP <43698882+ScrewedUP@users.noreply.github.com>
2026-08-03 17:14:14 +0530

    Time: 43 ms (36.65%), Space: 142.4 MB (31.45%) - LeetHub

---
+++ 1406-stone-game-iii/1406-stone-game-iii.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int solve(int i, vector<int>& nums, vector<int>& dp) {
+        if (i >= nums.size()) return 0;
+
+        if (dp[i] != INT_MIN) return dp[i];
+
+        int take1 = nums[i] - solve(i + 1, nums, dp);
+
+        int take2 = -10000;
+        if (i + 1 < nums.size())
+            take2 = nums[i] + nums[i + 1] - solve(i + 2, nums, dp);
+
+        int take3 = -10000;
+        if (i + 2 < nums.size())
+            take3 = nums[i] + nums[i + 1] + nums[i + 2] - solve(i + 3, nums, dp);
+
+        return dp[i] = max(take1, max(take2, take3));
+    }
+
+    string stoneGameIII(vector<int>& stoneValue) {
+        vector<int> dp(stoneValue.size() + 1, INT_MIN);
+
+        int x = solve(0, stoneValue, dp);
+
+        if (x > 0) return "Alice";
+        else if (x == 0) return "Tie";
+        return "Bob";
+    }
+};
