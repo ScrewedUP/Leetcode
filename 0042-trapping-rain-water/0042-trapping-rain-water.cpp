fa89ecf591f0a2b6d257d0b26b7098734711e6be
ScrewedUP <43698882+ScrewedUP@users.noreply.github.com>
2026-08-09 17:26:43 +0530

    Time: 0 ms (100%), Space: 25.8 MB (93.86%) - LeetHub

---
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int trap(vector<int>& h) {
+        int n = h.size();
+        int l = 0;
+        int r = n - 1;
+        int ans = 0;
+        int leftMax = 0,rightMax = 0;
+
+        while( l <= r ){
+            if ( h[l] <= h[r]){
+                if ( h[l] >= leftMax){
+                    leftMax = h[l];
+                }
+                else{
+                    ans += leftMax - h[l];
+                }
+                l++;
+            }
+            else{
+                if ( h[r] >= rightMax){
+                    rightMax = h[r];
+                }
+                else ans += rightMax - h[r];
+                r--;
+            }
+        }
+        return ans;
+    }
+};
