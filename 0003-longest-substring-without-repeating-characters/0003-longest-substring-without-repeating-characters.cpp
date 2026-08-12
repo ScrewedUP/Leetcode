a501baa28b2bc3ab1923ade1028eb10a884c7be0
ScrewedUP <43698882+ScrewedUP@users.noreply.github.com>
2026-08-12 19:07:52 +0530

    Time: 97 ms (13.72%), Space: 19.6 MB (13.59%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,20 +1,19 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        int l = 0;
         map<int,int> m;
-        int maxi = 0;
+        int l = 0;
+        int ans = 0;
         for(int i = 0 ; i < s.size() ; i++){
-            if ( m.find(s[i]) != m.end()){
-                while(m[s[i]] > 0 ){
-                    m[s[l]]--;
-                    if ( m[s[l]] == 0) m.erase(s[l]);
-                    l++;
-                }
-            }
             m[s[i]]++;
-            maxi = max(maxi,i-l+1);
+
+            while ( m[s[i]] > 1){
+                m[s[l]]--;
+                l++;
+            }
+
+            ans = max(ans,i-l+1);
         }
-        return maxi;
+        return ans;
     }
 };
