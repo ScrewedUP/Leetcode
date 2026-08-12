a361bcd27e274b47b5a2f9224e0544dbdfd4344a
ScrewedUP <43698882+ScrewedUP@users.noreply.github.com>
2026-08-12 20:13:41 +0530

    Time: 3 ms (65.92%) | Memory: 10.1 MB (31.09%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.cpp
+++ 567-permutation-in-string/permutation-in-string.cpp
@@ -1,30 +1,24 @@
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
-        int m = s1.size();
-        int n = s2.size();
-        if(m>n){
-            return false;
-        }
-        vector<int>smpp(26,0);
-        vector<int>pmpp(26,0);
-        int i = 0;
-        int j = 0;
-        while(i<m){
-            smpp[s2[i]-'a']++;
-            pmpp[s1[i]-'a']++;
-            i++;
-        }
-        if(smpp == pmpp){
-            return true;    
-        }
-        while(i<n){
-            smpp[s2[j++]-'a']--;
-            smpp[s2[i++]-'a']++;
-            if(smpp == pmpp){
-                return true;
+        vector<int> a(26,0);
+        vector<int> b(26,0);
+
+        for(auto c : s1) a[c-'a']++;
+        int x = s1.size();
+
+        int l = 0;
+        for(int i = 0 ; i < s2.size() ; i++){
+            b[s2[i] - 'a']++;
+
+            if ( i - l + 1 == x){
+                if ( a == b ) return true;
+                b[s2[l] - 'a']--;
+                l++;
             }
         }
         return false;
+
+        return true;
     }
 };
