5d37cf7f58e07be526bfeea160f576364c542f3e
ScrewedUP <43698882+ScrewedUP@users.noreply.github.com>
2026-08-23 19:24:44 +0530

    Time: 34 ms (40.96%) | Memory: 106.7 MB (84.27%) - LeetSync

--- 2245-destroying-asteroids/destroying-asteroids.cpp
+++ 2245-destroying-asteroids/destroying-asteroids.cpp
@@ -3,10 +3,8 @@ public:
     bool asteroidsDestroyed(int mass, vector<int>& a) {
         sort(a.begin(),a.end());
         long long x = mass;
-        for(int i = 0 ; i < a.size() ; i++){
-            if ( x >= a[i]){
-                x += a[i];
-            }
+        for(auto& i : a){
+            if ( x >= i ) x += i;
             else return false;
         }
         return true;
